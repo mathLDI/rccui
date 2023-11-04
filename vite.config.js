e3bc314ca75affddf0cfd223616026e5fd064ce3
Mathieu Laurin <laurinmathieu@hotmail.com>
2023-11-03 21:16:21 -0400

    Github pages works!!

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rccui/',
   plugins: [react()],
+  build: {
+    outDir: 'build', // Define the output directory for your build
+  },
 })
